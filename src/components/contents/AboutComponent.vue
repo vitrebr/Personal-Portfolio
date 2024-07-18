7683b05c5a2e479af239d99d8cc4b32acfca6387
Viant Trestan Brinto <77541868+vitrebr@users.noreply.github.com>
2024-07-18 08:59:26 +0000

    second commit

--- src/components/contents/AboutComponent.vue
+++ src/components/contents/AboutComponent.vue
@@ -3,7 +3,7 @@
         <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">About</span> Me</h1>
         <p class="leading-relaxed text-justify max-sm:text-sm">
             <span class="block mb-1">
-                Sebagai mahasiswa akhir yang akan lulus dengan semangat yang menyala-nyala dalam dunia pengembangan web, saya siap untuk membawa semangat baru dan perspektif yang segar ke dalam industri. Dengan latar belakang pendidikan saya dalam bidang teknik informatika dan kesukaan saya dalam merancang tampilan yang menarik, saya bersemangat untuk menjelajahi dunia frontend.
+                Sebagai mahasiswa semester 5 dengan semangat yang menyala-nyala dalam dunia pengembangan web, saya siap untuk membawa semangat baru dan perspektif yang segar ke dalam industri. Dengan latar belakang pendidikan saya dalam bidang teknik informatika dan kesukaan saya dalam merancang tampilan yang menarik, saya bersemangat untuk menjelajahi dunia frontend.
             </span>
             <span class="block mb-1">
                 Dengan pengetahuan saya tentang HTML, CSS, dan JavaScript yang saya peroleh selama kuliah dan pengalaman dalam membuat project kecil, saya siap untuk menciptakan antarmuka yang menawan dan responsif. Saya memiliki keinginan untuk terus belajar dan mengembangkan keterampilan saya, serta siap untuk menghadapi tantangan baru yang muncul di dunia pengembangan web khususnya dibidang frontend.
--- src/components/contents/EducationComponent.vue
+++ src/components/contents/EducationComponent.vue
@@ -2,9 +2,9 @@
     <div class="slide-right w-full rounded-md bg-background sm:p-5 max-sm:p-3 shadow-custom">
         <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">Education</span></h1>
         <ul class="flex flex-col space-y-1 list-disc pl-5 sm:pr-3 max-sm:text-sm">
-            <li>SDN 2 Biak Muli.</li>
-            <li>SMPs Darul Amin.</li>
-            <li>SMAs Hidatatullah (IPS).</li>
+            <li>SDN 076103 Bawozaua.</li>
+            <li>SMPN 1 Teluk Dalam.</li>
+            <li>SMAS Katolik Bintang Laut (IPA).</li>
             <li>Universitas Budi Darma Medan.</li>
         </ul>
     </div>
