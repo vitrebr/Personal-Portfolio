50f56c8ae299ef682f0cfa8e5275025579e82f71
Viant Trestan Brinto <77541868+vitrebr@users.noreply.github.com>
2024-07-24 13:19:51 +0000

    update

--- src/components/contents/ExperienceComponent.vue
+++ src/components/contents/ExperienceComponent.vue
@@ -3,13 +3,7 @@
         <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">Experience</span></h1>
         <ul class="flex flex-col space-y-1 list-disc pl-5 sm:pr-3 max-sm:text-sm">
             <li>Ketua Organinasi</li>
-            <p class="text-sm">Pernah menjabat selama 2 tahun (4 semester) sebagai ketua di organisasi Budidarma Programming Club dari tahun 2022 sampai tahun 2024.</p>
-            <li>Instruktur Frontend</li>
-            <p class="text-sm">Menjadi mentor atau instruktur pada organisasi Budidarma Programming Club pada tahun 2023 selama 1 tahun (2 semester).</p>
-            <li>Mentor Mini-Workshop</li>
-            <p class="text-sm">Pernah menjadi mentor pada acara Mini-Workshop yang di adakan oleh Budidarma Programming Club.</p>
-            <li>Panitia Seminar</li>
-            <p class="text-sm">Pernah menjadi panitia pada acara seminar yang diadakan oleh kampus.</p>
+            <p class="text-sm">Menjabat sebagai wakil ketua di organisasi Budidarma Programming Club mulai dari tahun 2024 ini.</p>
         </ul>
     </div>
 </template>
