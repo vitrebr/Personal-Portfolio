2e1e2fcd4e62e381cbfb778e45ca3063555b7ace
Viant Trestan Brinto <77541868+vitrebr@users.noreply.github.com>
2024-07-24 14:04:52 +0000

    update

--- src/components/contents/ExperienceComponent.vue
+++ src/components/contents/ExperienceComponent.vue
@@ -2,7 +2,7 @@
     <div class="slide-right w-full rounded-md bg-background sm:p-5 max-sm:p-3 shadow-custom">
         <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">Experience</span></h1>
         <ul class="flex flex-col space-y-1 list-disc pl-5 sm:pr-3 max-sm:text-sm">
-            <li>Ketua Organinasi</li>
+            <li>Wakil Ketua Organinasi BPC</li>
             <p class="text-sm">Menjabat sebagai wakil ketua di organisasi Budidarma Programming Club mulai dari tahun 2024 ini.</p>
         </ul>
     </div>
